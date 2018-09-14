233677ad02273d1730bc8ce121288ff68fdcd0f4
joefizz <joseph.rapley@hotmail.com>
2018-09-14 09:46:19 +0100

    Create downloader.php

---
+++ downloader.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<script src="jquery-3.3.1.min.js"></script>
+<script src="checkall.js"></script>
+</head>
+<P>
+<body>
+<H3>CE Plus Test Files for end user device checks
+</H3>
+<p>
+
+<?php
+        $files = scandir("/var/www/CEPlus/files");
+        foreach ($files as $file) {
+                if ($file != "." AND $file != ".."){
+                        echo "<a href='/CEPlus/files/$file'>$file<br>";
+                }
+        }
+?>
+<br>
